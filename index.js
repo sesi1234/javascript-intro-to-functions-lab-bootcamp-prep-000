1e52bca90d62d0b809cd2a7fc939d5e54530f77f
sesi_prosper@yahoo.co.uk <sesi_prosper@yahoo.co.uk>
2018-04-21 19:46:25 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,15 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(val) {
+  return val.toLowerCase()
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase()); 
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
