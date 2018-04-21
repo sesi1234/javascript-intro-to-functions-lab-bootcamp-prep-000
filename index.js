98e3f167a9687c662150881e3852fba0f7bb3f21
sesi_prosper@yahoo.co.uk <sesi_prosper@yahoo.co.uk>
2018-04-21 19:50:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,6 +14,6 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 
-function logWhisper(string) {
-  console.log(string.toLowerCase())
+function sayHiToGrandma(string) {
+  if (string.toUpperCase()===)
 }
