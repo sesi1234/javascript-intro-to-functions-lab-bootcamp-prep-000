d761a2dcf7704ad9ccba53da8f167999aba67475
sesi_prosper@yahoo.co.uk <sesi_prosper@yahoo.co.uk>
2018-04-21 19:55:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if (string.toUpperCase()===string){
     return "YES INDEED!"
-  }else(string.toLowerCase()===string){
-    return 
-  }
+  }else if(string.toLowerCase()===string){
+    return "I can't hear you!"
+  }else(string===)
 }
